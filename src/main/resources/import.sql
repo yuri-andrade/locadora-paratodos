6bcd8375133342483fc4d505b7b29434234717af
yuri.arend <yuri.arend@acad.pucrs.br>
2019-02-10 21:26:18 -0200

    Add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into filme (id, diretor, nome, locado) values (1, 'Tarantino', 'Pulp Fiction', 0);
+insert into filme (id, diretor, nome, locado) values (2, 'M Night S.', 'The Sixth Sense', 1);
+insert into filme (id, diretor, nome, locado) values (3, 'M Night S.', 'Signs', 1);
+insert into filme (id, diretor, nome, locado) values (4, 'M Night S.', 'A Vila', 1);
+insert into filme (id, diretor, nome, locado) values (5, 'M Night S.', 'A Vila', 1);
