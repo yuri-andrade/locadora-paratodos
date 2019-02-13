15ffff57d24009c70a4e1b5392ea80674fcca4c1
yuri.arend <yuri.arend@acad.pucrs.br>
2019-02-13 18:56:28 -0200

    Added unit tests

--- src/test/java/com/desafio/locadora/LocadoraApplicationTests.java
+++ src/test/java/com/desafio/locadora/LocadoraApplicationTests.java
@@ -2,12 +2,18 @@ package com.desafio.locadora;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class LocadoraApplicationTests {
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void contextLoads() {
---
+++ src/test/java/com/desafio/locadora/service/FilmeServiceTest.java
@@ -0,0 +1,79 @@
+package com.desafio.locadora.service;
+
+import com.desafio.locadora.converter.FilmeToFilmeOutConverter;
+import com.desafio.locadora.entity.Filme;
+import com.desafio.locadora.entity.enums.LocacaoEnum;
+import com.desafio.locadora.exception.ResourceNotFoundException;
+import com.desafio.locadora.repository.FilmeRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FilmeServiceTest {
+
+    @Mock
+    private FilmeRepository filmeRepository;
+
+    @Mock
+    private FilmeToFilmeOutConverter filmeToFilmeOutConverter;
+
+    private FilmeService filmeService;
+    private Filme filme;
+    private String nome = "Se7en";
+    private String diretor = "David Fincher";
+    private Long id = 1L;
+
+    @Before
+    public void init() {
+        filmeService = new FilmeService(filmeRepository, filmeToFilmeOutConverter);
+        filme = new Filme();
+        filme.setId(id);
+        filme.setNome(nome);
+        filme.setDiretor(diretor);
+        filme.setLocado(LocacaoEnum.NAO);
+    }
+
+    @Test
+    public void testFindByNome() {
+        List<Filme> list = new ArrayList<>();
+        list.add(filme);
+        Mockito.when(filmeRepository.findByNome(nome)).thenReturn(java.util.Optional.of(list));
+        List<Filme> retrivedList = filmeService.findByNome(nome);
+        Assert.assertEquals(list, retrivedList);
+    }
+
+    @Test
+    public void testIsAvailableTrue() {
+        Boolean retrievedBool = filmeService.isAvailable(filme);
+        Assert.assertEquals(true, retrievedBool);
+    }
+
+    @Test
+    public void testIsAvailableFalse() {
+        filme.setLocado(LocacaoEnum.SIM);
+        Boolean retrievedBool = filmeService.isAvailable(filme);
+        Assert.assertEquals(false, retrievedBool);
+    }
+
+    @Test
+    public void testFindByIdSucess() {
+        Mockito.when(filmeRepository.findById(id)).thenReturn(java.util.Optional.ofNullable(filme));
+        Filme retrivedFilme = filmeService.findById(id);
+        Assert.assertEquals(filme, retrivedFilme);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void testFindByIdFail() {
+        filmeService.findById(2L);
+    }
+
+
+}
